ce2e03cf094c1488a24ed72eb9ab419409db34b8
oleg@ongoza.com <oleg@ongoza.com>
2019-01-07 15:05:41 +0200

    new file:   Assets/Resources/Data.cs 	new file:   Assets/Resources/camMaterial.mat 	new file:   Assets/Resources/camProtector.fbx 	new file:   Assets/Resources/timedReticle.shader

---
+++ Assets/Resources/Data.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public class Data {
+
+    private static readonly Dictionary<string, string> MessagesEng = new Dictionary<string, string>() {
+        { "Intro", "Error One" },
+        { "2", "Error Two" }
+    };
+
+    private static readonly Dictionary<string, string> MessagesRus = new Dictionary<string, string>() {
+        { "Intro", "Старт программы" },
+        { "2", "вторая часть" }
+    };
+
+    public static string getMessage(string key)
+    {
+        string result = "";
+        result = MessagesRus[key];
+        return result;
+    }
+}
