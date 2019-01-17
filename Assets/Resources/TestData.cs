5fd77a9ca45a43a3c8293275bcc2e827f50fa464
oleg@ongoza.com <oleg@ongoza.com>
2019-01-17 16:35:23 +0200

    new file:   Assets/Resources/TestData.cs

---
+++ Assets/Resources/TestData.cs
@@ -0,0 +1,18 @@
+﻿[System.Serializable] public class TestData{
+    public string startDateTime;
+    public string deviceID;    
+    public int test2Time;
+    public string email;
+    public int[,] testsResult;
+    public SnenaMotionData[] snenaMotionData;
+}
+
+[System.Serializable] public class SnenaMotionData{
+    public int sceneIndex;
+    public UserActivities[] userActivities;
+}
+
+[System.Serializable] public class UserActivities {
+    public float time;
+    public float[] activity;
+}
